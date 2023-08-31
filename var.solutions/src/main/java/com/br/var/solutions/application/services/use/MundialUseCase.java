5ea1cc4f15b17a3a0fab31b4cc8dc827524d98b1
Lucas Akio <107128189+akiosantos@users.noreply.github.com>
2023-08-31 19:20:41 -0300

    Update MundialUseCase.java

--- var.solutions/src/main/java/com/br/var/solutions/application/services/use/MundialUseCase.java
+++ var.solutions/src/main/java/com/br/var/solutions/application/services/use/MundialUseCase.java
@@ -1,6 +1,27 @@
-package com.br.var.solutions.application.services.use;
+package com.br.var.solutions.application.services.impl;
 
-public interface MundialUseCase {
+import com.br.var.solutions.application.services.use.MundialUseCase;
+import org.springframework.stereotype.Service;
 
-    String calculoMundial(String time);
+@Service
+public class MundialUseCaseImpl implements MundialUseCase {
+
+    public String calculoMundial(String time){
+        return calcularMundial(time);
+    }
+
+
+
+    private String calcularMundial(String time) {
+
+        if (time.equalsIgnoreCase("Corinthias")) {
+            return "Parabéns, o seu time possui 2 mundiais de clubes conforme a FIFA";
+        } else if (time.equalsIgnoreCase("São Paulo")) {
+            return "Parabéns, o seu time possui 3 mundiais de clubes conforme a FIFA";
+        } else if (time.equalsIgnoreCase("Santos")) {
+            return "Parabéns, o seu time possui 2 mundiais de clubes conforme a FIFA";
+        } else {
+            return "poxa, que pena. Continue torcendo para o seu clube ganhar o mundial";
+        }
+    }
 }
